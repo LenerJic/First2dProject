1cfac7860a42a1d7fcee4411fc5595a156f06cbc
LenerJic <jleneringa10@gmail.com>
2026-09-11 23:58:39 -0500

    Monedas, Contador y Musica

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,15 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
+    private static readonly int IsGroundedHash = Animator.StringToHash("IsGrounded");
+    private static readonly int VerticalVelocityHash = Animator.StringToHash("VerticalVelocity");
+    private static readonly int SpeedHash = Animator.StringToHash("Speed");
     private Rigidbody2D rb2D;
     public float speed = 2f;
     private Vector2 moveDirection;
@@ -14,9 +21,19 @@ public class Player : MonoBehaviour
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
 
+    private Animator animator;
+
+    private int coins;
+    public TMP_Text textCoins;
+
+    public AudioSource audioSource;
+    public AudioClip coinClip;
+    public AudioClip barrelClip;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void OnEnable()
@@ -45,6 +62,43 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         rb2D.linearVelocity = new Vector2(moveDirection.x * speed, rb2D.linearVelocity.y);
+        animator.SetFloat(SpeedHash, Mathf.Abs(moveDirection.x));
+        animator.SetFloat(VerticalVelocityHash, rb2D.linearVelocity.y);
+        animator.SetBool(IsGroundedHash, isGrounded);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Coin"))
+        {
+            audioSource.PlayOneShot(coinClip);
+            Destroy(collision.gameObject);
+            coins++;
+            textCoins.text = coins.ToString();
+        }
+
+        if (collision.transform.CompareTag("Spikes"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (collision.transform.CompareTag("Barrel"))
+        {
+            audioSource.PlayOneShot(barrelClip);
+            Vector2 knockbackDir = (rb2D.position - (Vector2)collision.transform.position).normalized;
+            rb2D.linearVelocity = Vector2.zero;
+            rb2D.AddForce(knockbackDir * 3, ForceMode2D.Impulse);
+
+            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
+
+            foreach (BoxCollider2D col in colliders)
+            {
+                col.enabled = false;
+            }
+
+            collision.GetComponent<Animator>().enabled = true;
+            Destroy(collision.gameObject, 0.5f);
+        }
     }
 
     private void Jump()
